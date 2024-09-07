368801652a6946ebe2cc3bdfe81497949e61440c
RakibHossen678 <rh491464@gmail.com>
2024-09-07 17:00:25 +0600

    added review section

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Review from "@/components/Review";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <span className="font-bold text-2xl">ProServe</span>
         <Button>Login</Button>
       </main>
-
+      <Review />
       <footer className="bg-slate-900 text-white">
         <div className="container  py-20">footer</div>
       </footer>
