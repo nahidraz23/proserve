c52c96218e47d478d765953131212eb56616cafc
nahidraz23 <nzamanraz@gmail.com>
2024-09-07 17:52:50 +0600

    duplicate navbar and footer removed, added by rakib vai

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,7 @@ import Review from "@/components/Review";
 export default function Home() {
   return (
     <div className="">
-      <main className="flex justify-between container py-4">
-        <span className="font-bold text-2xl">ProServe</span>
-        <Button>Login</Button>
-      </main>
       <Review />
-      <footer className="bg-slate-900 text-white">
-        <div className="container  py-20">footer</div>
-      </footer>
     </div>
   );
 }
