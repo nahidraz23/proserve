4db414665e43ecadfc20c4d5fa01a67ae1eb74ab
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-09-08 17:45:58 +0600

    Riyaz: fixed navbar z-index

--- src/components/HomePage/Header/Header.jsx
+++ src/components/HomePage/Header/Header.jsx
@@ -2,12 +2,11 @@ import SignIn from "@/components/Modal/SignIn";
 import SignUp from "@/components/Modal/SignUp";
 import MobileNav from "@/components/Shared/Navbar/MobileNav";
 import Navbar from "@/components/Shared/Navbar/Navbar";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="py-5 bg-black fixed w-full opacity-80 flex">
+    <header className="py-5 bg-black fixed z-50 w-full opacity-80 flex">
       <div className="flex justify-between container">
         <Link href="/" className="font-bold text-2xl text-white">
           ProServe<span className="text-primary">.</span>
