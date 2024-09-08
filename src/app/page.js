d906ee78d55fff6891df3224b023e3371ac4acd1
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-09-08 21:33:00 +0600

    Mahfuz - experiences section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Review from "@/components/Review";
 import Banner from "@/components/HomePage/Banner/Banner";
+import Experiences from "@/components/HomePage/experiences/Experiences";
 
 export default function Home() {
   return (
     <div className="">
       <Banner></Banner>
+      <Experiences />
       <Review />
     </div>
   );
