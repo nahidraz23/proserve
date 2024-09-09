1ba50180c813d89d0153e084cad2bf66bf597b3d
nahidraz23 <nzamanraz@gmail.com>
2024-09-09 22:42:17 +0600

    Nahid: recommend service

--- src/components/HomePage/RecommendedServiceSection/Recommendedservice.jsx
+++ src/components/HomePage/RecommendedServiceSection/Recommendedservice.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card } from '@/components/ui/card';
 import Image from 'next/image';
+import ac from '../../../assets/images/recommended/acservice.jpg'
+import home from '../../../assets/images/recommended/homecleaning.jpg'
+import ambulance from '../../../assets/images/recommended/ambulance.jpg'
+import caregiver from '../../../assets/images/recommended/caregiver.png'
+import pestcontrol from '../../../assets/images/recommended/pestcontrol.jpg'
+import { Select } from '@/components/ui/select';
 
 const Recommendedservice = () => {
+
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
     return (
         <div className='flex flex-col items-center'>
             <div className='mt-8 md:mt-32'>
                 <h1 className='text-5xl font-bold'>Recommended Service</h1>
             </div>
             <div className='mt-8'>
-                <Tabs defaultValue="account" className="w-full">
+                <Tabs className="w-full" defaultValue="ac-servicing">
                     <TabsList className="bg-[#EEE]">
                         <TabsTrigger value="ac-servicing">AC Servicing</TabsTrigger>
                         <TabsTrigger value="home-cleaning">Home Cleaning</TabsTrigger>
@@ -18,29 +28,54 @@ const Recommendedservice = () => {
                         <TabsTrigger value="caregiver">Caregiver</TabsTrigger>
                         <TabsTrigger value="pest-Control">Pest Control</TabsTrigger>
                     </TabsList>
-                    <TabsContent value="ac-servicing">
-                        <Card>
-                            <h1 className='text-center font-bold'>AC Servicing</h1>
-                        </Card>
+                    <TabsContent value="ac-servicing" >
+                            <Card>
+                                <Image
+                                    src={ac}
+                                    width={614}
+                                    height={300}
+                                    className='rounded-xl'
+                                ></Image>
+                            </Card>
                     </TabsContent>
                     <TabsContent value="home-cleaning">
                         <Card>
-                            <h1 className='text-center font-bold'>Home Cleaning</h1>
+                            <Image
+                                src={home}
+                                width={614}
+                                height={300}
+                                className='rounded-xl'
+                            ></Image>
                         </Card>
                     </TabsContent>
                     <TabsContent value="ambulance">
                         <Card>
-                            <h1 className='text-center font-bold'>Ambulance</h1>
+                            <Image
+                                src={ambulance}
+                                width={614}
+                                height={300}
+                                className='rounded-xl'
+                            ></Image>
                         </Card>
                     </TabsContent>
                     <TabsContent value="caregiver">
                         <Card>
-                            <h1 className='text-center font-bold'>Caregiver</h1>
+                            <Image
+                                src={caregiver}
+                                width={614}
+                                height={300}
+                                className='rounded-xl'
+                            ></Image>
                         </Card>
                     </TabsContent>
                     <TabsContent value="pest-Control">
                         <Card>
-                            <h1 className='text-center font-bold'>Pest Control</h1>
+                            <Image
+                                src={pestcontrol}
+                                width={614}
+                                height={300}
+                                className='rounded-xl'
+                            ></Image>
                         </Card>
                     </TabsContent>
                 </Tabs>
