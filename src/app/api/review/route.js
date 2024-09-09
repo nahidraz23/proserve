8258bbe1c33e9f2de5ba9266563b3c0c4ae88caa
nahidraz23 <nzamanraz@gmail.com>
2024-09-09 23:20:14 +0600

    Nahid: trying to fix deply issue

--- src/app/api/review/route.js
+++ src/app/api/review/route.js
@@ -6,7 +6,7 @@ export const GET = async (request) => {
     const db = await connectDB();
     const reviewsCollection = db.collection("review");
     const reviews = await reviewsCollection.find().toArray();
-    return NextResponse.json({ reviews });
+    return NextResponse.json(reviews);
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "No Data Found", error });
