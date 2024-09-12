9ddb21d8da0aa0c0ab664111ae0712d8a44333d9
nahidraz23 <nzamanraz@gmail.com>
2024-09-12 15:43:28 +0600

    Merge issue fixed

--- src/components/HomePage/RecommendedServiceSection/Recommendedservice.jsx
+++ src/components/HomePage/RecommendedServiceSection/Recommendedservice.jsx
@@ -1,37 +1,3 @@
-
-import React from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
-const RecommendedService = () => {
-  return (
-    <div className="flex flex-col items-center">
-      <div className="mt-8 md:mt-32">
-        <h1 className="text-5xl font-bold">Recommended Service</h1>
-      </div>
-      <div className="mt-8">
-        <Tabs defaultValue="account" className="w-full">
-          <TabsList className="bg-[#EEE]">
-            <TabsTrigger value="ac-servicing">AC Servicing</TabsTrigger>
-            <TabsTrigger value="home-cleaning">Home Cleaning</TabsTrigger>
-            <TabsTrigger value="ambulance">Ambulance</TabsTrigger>
-            <TabsTrigger value="caregiver">Caregiver</TabsTrigger>
-            <TabsTrigger value="pest-Control">Pest Control</TabsTrigger>
-          </TabsList>
-          <TabsContent value="ac-servicing">
-            Make changes to your account here.
-          </TabsContent>
-          <TabsContent value="home-cleaning">Home Cleaning.</TabsContent>
-          <TabsContent value="ambulance">Ambulance service.</TabsContent>
-          <TabsContent value="caregiver">Caregiver service.</TabsContent>
-          <TabsContent value="pest-Control">Pest contorl service.</TabsContent>
-        </Tabs>
-      </div>
-    </div>
-  );
-};
-
-export default RecommendedService;
-
 'use client'
 import React, { useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
@@ -42,11 +8,8 @@ import home from '../../../assets/images/recommended/homecleaning.jpg'
 import ambulance from '../../../assets/images/recommended/ambulance.jpg'
 import caregiver from '../../../assets/images/recommended/caregiver.png'
 import pestcontrol from '../../../assets/images/recommended/pestcontrol.jpg'
-import { Select } from '@/components/ui/select';
 
 const Recommendedservice = () => {
-
-    const [selectedIndex, setSelectedIndex] = useState(0);
 
     return (
         <div className='flex flex-col items-center'>
