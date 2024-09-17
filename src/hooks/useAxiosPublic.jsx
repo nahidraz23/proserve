e0f24c55d7bc409e614b59319c739ee10987b8dd
nahidraz23 <nzamanraz@gmail.com>
2024-09-17 18:59:04 +0600

    data fetching url updated

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axipsPublic = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://proserve-three.vercel.app'
 })
 
 const useAxiosPublic = () => {
